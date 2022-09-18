1b3cb7f49a9814a72a76bb8fa7e7c357133f3199
Soumyajit Saha <44563233+soumyajitsaha72@users.noreply.github.com>
2022-09-18 16:32:35 +0530

    v0.3(Refatored the code)

--- src/app/home/skills/skills.component.ts
+++ src/app/home/skills/skills.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills: string[] = ["HTML", "CSS", "Javascript", "TypeScript", "Angular", "Java", "Spring Boot"];
+  skillsUsedBefore: string[] = ["MongoDb", "React"];
+  skillsWantToLearn: string[] = ["Vue", "GraphQL", "MongoDb", "React"];
+
   constructor() { }
 
   ngOnInit(): void {
